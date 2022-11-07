ea9c9eab1b51d17f523c88ca6c70add49a1ad23b
raju-miah <rajumiahpollob7@gmail.com>
2022-11-08 02:49:07 +0600

    added home banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='home'>
+            <div className='home-text'>
+                <h1 className='my-name'>Alex Smith</h1>
+                <p>______ a Freelance Photographer</p>
+                <p>I'm photographer based in New Work, and I'm <br />
+                    very dedicated to my work</p>
+                <p className='my-solo'>"Photography in not to create a moment, <br />Photography is capture the moment"</p>
+            </div>
+            <div className='home-img'>
+                <img src="https://i.ibb.co/LPcFzX8/home-img.png" alt="" />
+            </div>
         </div>
     );
 };
