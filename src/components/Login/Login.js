e75c535c8bc2de284e7fb2fc351d4461287f8acf
raju-miah <rajumiahpollob7@gmail.com>
2022-11-08 14:59:55 +0600

    style login firld

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login</h2>
+        <div className='from-container'>
+            <h2 className='login-text'>Login</h2>
+            <form className='form'>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <input className='btn-login' type="submit" value="Login" />
+            </form>
+            <p className='new-u'>New to my photography site? <Link to="/signup">Please SignUp</Link></p>
         </div>
     );
 };
